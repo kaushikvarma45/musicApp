4b029afede05070d56bd527c9753e9be7642f359
kaushik varma <39562315+kaushikvarma45@users.noreply.github.com>
2022-07-09 16:23:03 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,8 +4,7 @@ import pandas as pd
 import numpy as np
 import os
 from tensorflow.keras.models import Sequential, model_from_json
-# import sounddevice as sd
-# from scipy.io.wavfile import write
+
 
 
 
@@ -80,34 +79,7 @@ def show_predict_page():
     elif record:
       fs = 44100  # Sample rate
       seconds = 5  # Duration of recording
-#       myrecording = sd.rec(int(seconds * fs), samplerate=fs, channels=1)
-#       sd.wait()  # Wait until recording is finished
-#       write('output.wav', fs, myrecording)  # Save as WAV file
-#       song_name = 'output.wav'
-#       song_pieces = prepare_song(song_name)
-#       all_tracks = song_pieces
-#       genre = ([0]*len(song_pieces))
 
-#       print("actual val : " + str(genre[0]+1))
-#       pops = np.array(all_tracks)
-#       pops = pops.reshape(-1,128,98,1)
-#       predval = loaded_model.predict(pops)
-#       classcase = 0
-#       predicted = [0,0,0]
-#       for i in range(len(predval)):
-#           arr = predval[i]
-#           index = np.where(arr == np.amax(arr))
-#           predicted[index[0][0]]+=1
-#       for i in range(3):
-#           ff = (100*predicted[i])/len(predval)
-#           ff = (round(ff, 2))
-#           if ff>=50 : 
-#               classcase = i+1
-#           print("Class " + str(i+1) + " Probab = " + str(ff) + "%")
-#           st.subheader("Class " + str(i+1) + " Probab = " + str(ff) + "%")
-
-#       # salary = regressor.predict(X)
-#       st.subheader(f"The estimated class is {classcase}")
 
 
 show_predict_page()
